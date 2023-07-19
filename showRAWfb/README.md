505ee67a037d2fdcb0571368dba94653054773ec
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-07-19 21:11:41 +0200

    [showRAWfb] add app to display RAW /dev/fb0 img
    from 320x240 screen pixel data

---
+++ showRAWfb/README.md
@@ -0,0 +1,17 @@
+* **Description:**     
+Program to display copy of framebuffer data commonly know as `/dev/fb0` of 320x240px display
+
+* **source from:**  
+base source from [steward-fu/bootloader](  https://github.com/steward-fu/bootloader/blob/f1c100s_fc3000_uboot-2018.01/tools/show_320x240.c) git repo
+
+* **build instructions**   
+```
+make
+```
+
+* **usage:**  
+```
+./showRAWfb <image.raw>
+```
+  
+  binary compiled with dynamic MiyooCFW toolchain
---
+++ showRAWfb/show.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <unistd.h> 
+#include <fcntl.h>
+
+int main(int argc, char** argv)
+{
+    SDL_Surface* screen=NULL;
+
+    if(argc != 2){
+        printf("usage:\n\tshow_320x240 xxx\n");
+        return -1;
+    }
+ 
+    SDL_Init(SDL_INIT_VIDEO);
+    screen = SDL_SetVideoMode(320, 240, 16, SDL_SWSURFACE);
+    SDL_ShowCursor(SDL_DISABLE);
+
+    int fd = open(argv[1], O_RDONLY);
+    if(fd > 0){
+        read(fd, screen->pixels, 320*240*2);
+        close(fd);
+
+        SDL_Flip(screen);
+        SDL_Event event;
+        while(1){
+            if(SDL_PollEvent(&event)){
+                if(event.type == SDL_KEYDOWN){
+                    break;
+                }
+            }
+            usleep(100);
+        }
+    }
+    SDL_Quit();
+    return 0;
+}
