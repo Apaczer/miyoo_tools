94fcf567273cabfb7b194b4b778e8cc0b4a8337b
Apaczer <94932128+Apaczer@users.noreply.github.com>
2026-04-16 22:23:16 +0200

    [DOS]: dosbox_setup: add "Show DOSBOX.BAT" option
    + sanity checks

--- dos/dosbox_setup.sh
+++ dos/dosbox_setup.sh
@@ -9,47 +9,84 @@ fi
 
 content="$1"
 
+wait_msg_func() {
+	## read is different in POSIX shell (should work on BusyBox)
+	echo
+	read -n 1 -s -r -p "Press any key to continue"
+	echo
+}
+
 consent_msg_func() {
-	Option1="Continue"
-	Option2="Exit/Abort"
+	option1="Continue"
+	option2="Show DOSBOX.BAT" 
+	option3="Exit/Abort"
 
 	top_msg="This launcher creates DOSBOX.BAT in ${content}"
 	bottom_msg="   Select(hold): Exit        Start/Y/Right: Choose.  "
 
-	Mychoice=$( echo -e "$Option1\n$Option2\n$Option3\n$Option4\n$Option5"\
+	my_choice=$( echo -e "$option1\n$option2\n$option3" \
 				 | shellect -t "$top_msg" -b "$bottom_msg")
-	case "$Mychoice" in
-		"$Option1")
+	case "$my_choice" in
+		"$option1")
+			return;
+			;;
+		"$option2")
+			show_bat=yes
 			return;
 			;;
 		*) exit;;
 	esac
 }
 
-wait_msg_func() {
-	## read is different in POSIX shell (should work on BusyBox)
-	echo
-	read -n 1 -s -r -p "Press START to continue"
-	echo
+# shellcheck disable=SC2120 #doesn't detect piping in func.
+sellect_file_func() {
+	top_msg="Select a file to be called from the DOSBOX.BAT"
+	bottom_msg="   Select(hold): Exit        Start/Y/Right: Choose.  "
+
+	my_choice=$(shellect -t "$top_msg" -b "$bottom_msg" "$1")
+	#Sanity check for Escape press (thus empty output)
+	if ! test -z "${my_choice}"; then
+		echo "$my_choice"
+	else
+		exit
+	fi
 }
 
 consent_msg_func
 
 write_success=false
+read_success=false
 if test -d "$content"; then
 	#Directory path
-	find "${content}" -printf '%P\n' | shellect | sed 's:/:\\:g' | tee "${content}"/DOSBOX.BAT \
-	 && write_success=true
+	if test x"$show_bat" = "xyes"; then
+		cat DOSBOX.BAT \
+		 && read_success=true
+	else
+		find "${content}" -printf '%P\n' | shellect | sed 's:/:\\:g' | tee "${content}"/DOSBOX.BAT \
+		 && write_success=true
+	fi
 elif test -f "$content"; then
 	#test
-	if zip -T "$content" >/dev/null ; then
+	if zip -T "$content" >/dev/null 2>&1; then #BusyBox v1.35.0 bug: calling zip -T calls also uznzip with bad opt
 		#ZIP archive
-		unzip -l "${content}" | tr -s ' ' | cut -d' ' -f5 | grep -Ei "EXE|BAT|COM" | shellect | sed 's:/:\\:g' | tee DOSBOX.BAT && zip -m "${content}" DOSBOX.BAT >/dev/null \
-		 && write_success=true
-	elif 7zr t -bb0 -ba "${content}" >/dev/null 2>&1 ; then
+		if test x"$show_bat" = "xyes"; then
+			#BusyBox bug: the -p opt always return 0;
+			read_content="$(unzip -p "${content}" DOSBOX.BAT)"
+			! test -z "$read_content" \
+			 && { read_success=true ; echo "$read_content" ; }
+		else
+			unzip -l "${content}" | tr -s ' ' | cut -d' ' -f5 | grep -Ei "EXE|BAT|COM" | sellect_file_func | sed 's:/:\\:g' | tee DOSBOX.BAT && zip -m "${content}" DOSBOX.BAT >/dev/null \
+			 && write_success=true
+		fi
+	elif 7zr t -bb0 -ba "${content}" >/dev/null 2>&1; then
 		#7Z archive (exclude -f5 column with 0 size calculated?)
-		7zr l -ba "${content}" | sed 's/ \+/\t/g' | cut -f6 | grep -Ei "EXE|BAT|COM" | shellect | sed 's:/:\\:g' | tee DOSBOX.BAT && 7z a -sdel "${content}" DOSBOX.BAT >/dev/null \
-		 && write_success=true
+		if test x"$show_bat" = "xyes"; then
+			7zr e -so "${content}" DOSBOX.BAT \
+			 && read_success=true
+		else
+			7zr l -ba "${content}" | sed 's/ \+/\t/g' | cut -f6 | grep -Ei "EXE|BAT|COM" | sellect_file_func | sed 's:/:\\:g' | tee DOSBOX.BAT && 7z a -sdel "${content}" DOSBOX.BAT >/dev/null \
+			 && write_success=true
+		fi
 	else
 		echo -en "\nWARNING: The provided ${content} should be either directory or 7Z|ZIP archive file to generate/add DOSBOX.BAT"
 		wait_msg_func
@@ -59,7 +96,9 @@ fi
 
 if $write_success; then
 	echo -en "\nSuccesfuly generated DOSBOX.BAT in ${content}"
+elif $read_success; then
+	echo
 else
-	echo -en "\nERROR: there was an issue generating DOSBOX.BAT in ${content}..."
+	echo -en "\nERROR: there was an issue generating/reading DOSBOX.BAT in ${content}..."
 fi
 wait_msg_func
